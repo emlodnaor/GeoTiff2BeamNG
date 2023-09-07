2c77a49ceb38146aab41b652cd4a846a43a6a7c4
emlodnaor emlodnaor <emlod.naor@gmail.com>
2023-09-07 10:09:14 +0200

    First working version

---
+++ GeoTiff2BeamNG/BeamNGTerrainFileBuilder.cs
@@ -0,0 +1,162 @@
+﻿using OSGeo.GDAL;
+
+internal class BeamNGTerrainFileBuilder
+{
+    private string croppedOutputFile;
+
+    public BeamNGTerrainFileBuilder(string croppedOutputFile)
+    {
+        this.croppedOutputFile = croppedOutputFile;
+    }
+
+    internal async Task Build()
+    {
+        List<string> materialNames = new() //This should be dynamic!!!
+            {
+                "Grass2",
+                "Dirt",
+                "Mud",
+                "asphalt",
+                "ROCK",
+                "asphalt2"
+            };
+
+        var heightArray = GetHeightArray(croppedOutputFile);
+        WriteTerrainFile(heightArray, materialNames);
+
+
+    }
+    private static void WriteTerrainFile(double[,] heightArray, List<string> materialNames)
+    {
+        //data to the terrainfile is seemingly written to file startin lower left, to lower right, ending at upperright 
+        byte version = 8; // unsure if beamng render/map version, or version of the map
+
+        uint size = (uint)heightArray.GetLength(0);
+
+        var binaryWriter = new BinaryWriter(File.Open($@"C:\Users\emlodnaor\Source\Repos\GeoTiff2BeamNG\GeoTiff2BeamNG\bin\Debug\net7.0\theTerrain.ter", FileMode.Create));
+        binaryWriter.Write(version);
+        binaryWriter.Write(size);
+
+        WriteHeightMap(binaryWriter, heightArray);
+        WriteLayerMap(binaryWriter, heightArray);
+        WriteLayerTexture(binaryWriter, heightArray);
+
+        binaryWriter.Write(materialNames.Count);
+        WriteMaterialNames(binaryWriter, materialNames);
+
+        binaryWriter.Close();
+    }
+    private static void WriteMaterialNames(BinaryWriter binaryWriter, List<string> materialNames)
+    {
+        foreach (var materialName in materialNames)
+            binaryWriter.Write(materialName);
+    }
+
+    private static void WriteLayerTexture(BinaryWriter binaryWriter, double[,] heightArray)
+    {
+        foreach (var p in heightArray)
+            binaryWriter.Write(0);
+    }
+
+    private static void WriteLayerMap(BinaryWriter binaryWriter, double[,] heightArray)
+    {
+        var longitudes = heightArray.GetLength(0);
+        var latitudes = heightArray.GetLength(1);
+
+        var longitudeCounter = 0;
+        var latitudeCounter = 0;
+
+
+
+        while (latitudeCounter < latitudes)
+        {
+            byte theByte = 0;
+            binaryWriter.Write(theByte);
+
+            longitudeCounter++;
+
+            if (longitudeCounter > longitudes - 1) //unsure
+            {
+                longitudeCounter = 0;
+                latitudeCounter++;
+            }
+        }
+    }
+
+    private static void WriteHeightMap(BinaryWriter binaryWriter, double[,] heightArray)
+    {
+        var minAltitude = double.MaxValue;
+        var maxAltitude = double.MinValue;
+        foreach ( var height in heightArray ) 
+        { 
+            minAltitude = Math.Min(minAltitude,height);
+            maxAltitude= Math.Max(maxAltitude,height);
+        }
+        var heightDifference = maxAltitude - minAltitude;
+        var steps = 65535d;
+        var stepsPerMeter = steps / heightDifference;
+
+        var longitudeCounter = 0;
+        var latitudeCounter = 0;
+        var latitudes = heightArray.GetLength(1);
+
+        while (latitudeCounter < latitudes)
+        {
+            var localAltitude = heightArray[longitudeCounter, latitudeCounter] - minAltitude;
+            var binaryAltitude = localAltitude * stepsPerMeter;
+            ushort binaryInt = (ushort)Math.Round(binaryAltitude, 0);
+            binaryWriter.Write(binaryInt);
+
+            longitudeCounter++;
+
+            if (longitudeCounter > heightArray.GetLength(0) - 1)
+            {
+                longitudeCounter = 0;
+                latitudeCounter++;
+            }
+        }
+        Console.WriteLine($"Done setting heigh map. diffHeight: {heightDifference}");
+
+    }
+    
+
+    private double[,] GetHeightArray(string fileName)
+    {
+        // Open the GeoTIFF file
+        Dataset dataSet = Gdal.Open(fileName, Access.GA_ReadOnly);
+
+        // Get the raster band
+        Band band = dataSet.GetRasterBand(1);
+        band.AsMDArray();
+
+        // Get the dimensions of the raster
+        int width = dataSet.RasterXSize;
+        int height = dataSet.RasterYSize;
+
+        // Get the value of the pixels at a given x and y coordinate
+        var pixelValues = new double[width * height];
+
+        band.ReadRaster(0, 0, width, height, pixelValues, width, height, 0, 0); //
+
+        var resultPixelValues = new double[width, height];
+
+        //pictures are read from top left to bottom right, but we use lon and lat, so we read from bottom left to top right, this takes care of that... 
+
+        var longitudeCounter = 0;
+        var latitudeCounter = height - 1;
+
+        foreach (var value in pixelValues)
+        {
+            resultPixelValues[longitudeCounter, latitudeCounter] = value;
+
+            longitudeCounter++;
+            if (longitudeCounter > width - 1)
+            {
+                longitudeCounter = 0;
+                latitudeCounter--;
+            }
+        }
+
+        return resultPixelValues;
+    }
+}
---
+++ GeoTiff2BeamNG/GeoTiffCropper.cs
@@ -0,0 +1,105 @@
+﻿using OSGeo.GDAL;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GeoTiff2BeamNG
+{
+    internal class GeoTiffCropper
+    {
+        private void SetOutputProjection(Dataset outputDataset, string inputProjection)
+        {
+            outputDataset.SetProjection(inputProjection);
+        }
+
+        public async Task GeoTiffOutputExtractor(string filenameRawImg, string filenameImg, BoundaryBox inputBoundaryBox)
+        {
+            if (File.Exists(filenameImg)) { File.Delete(filenameImg); }
+            Gdal.AllRegister();
+
+            // Open the input GeoTIFF file
+            using (Dataset inputDataset = Gdal.Open(filenameRawImg, Access.GA_ReadOnly))
+            {
+                double[] geoTransform = new double[6];
+                inputDataset.GetGeoTransform(geoTransform);
+
+                double pixelSizeX = Math.Abs(geoTransform[1]);
+                double pixelSizeY = Math.Abs(geoTransform[5]);
+
+                int xOffset = CalculateXOffset(geoTransform, inputBoundaryBox);
+                int yOffset = CalculateYOffset(geoTransform, inputBoundaryBox, pixelSizeY);
+                int width = CalculateWidth(inputBoundaryBox, pixelSizeX);
+                int height = CalculateHeight(inputBoundaryBox, pixelSizeY);
+
+                Dataset outputDataset = CreateOutputDataset(filenameImg, width, height, inputDataset);
+
+                SetOutputGeoTransform(outputDataset, geoTransform, inputBoundaryBox);
+                SetOutputProjection(outputDataset, inputDataset.GetProjection());
+
+                CopyRasterData(inputDataset, outputDataset, xOffset, yOffset, width, height);
+
+                outputDataset.FlushCache();
+                outputDataset.Dispose();
+            }
+        }
+
+        private int CalculateXOffset(double[] geoTransform, BoundaryBox inputBoundaryBox)
+        {
+            return (int)((((double)inputBoundaryBox.MinimumLongitude - geoTransform[0]) / geoTransform[1]) / Math.Abs(geoTransform[1]));
+        }
+
+        private int CalculateYOffset(double[] geoTransform, BoundaryBox inputBoundaryBox, double pixelSizeY)
+        {
+            return (int)(((geoTransform[3] - (double)inputBoundaryBox.MaximumLatitude) / Math.Abs(geoTransform[5])) / pixelSizeY);
+        }
+
+        private int CalculateWidth(BoundaryBox inputBoundaryBox, double pixelSizeX)
+        {
+            return (int)((double)inputBoundaryBox.Width / pixelSizeX);
+        }
+
+        private int CalculateHeight(BoundaryBox inputBoundaryBox, double pixelSizeY)
+        {
+            return (int)((double)inputBoundaryBox.Height / pixelSizeY);
+        }
+
+        private Dataset CreateOutputDataset(string filenameImg, int width, int height, Dataset inputDataset)
+        {
+            return Gdal.GetDriverByName("GTiff").Create(
+                filenameImg,
+                width,
+                height,
+                inputDataset.RasterCount,
+                inputDataset.GetRasterBand(1).DataType,
+                null);
+        }
+
+        private void SetOutputGeoTransform(Dataset outputDataset, double[] geoTransform, BoundaryBox inputBoundaryBox)
+        {
+            outputDataset.SetGeoTransform(new double[]
+            {
+                (double)inputBoundaryBox.MinimumLongitude,
+                Math.Abs(geoTransform[1]), // Use adjusted pixel size
+                geoTransform[2],
+                (double)inputBoundaryBox.MaximumLatitude,
+                geoTransform[4],
+                -Math.Abs(geoTransform[5]) // Use adjusted pixel size with a negative sign
+            });
+        }
+
+        private void CopyRasterData(Dataset inputDataset, Dataset outputDataset, int xOffset, int yOffset, int width, int height)
+        {
+            for (int bandIndex = 1; bandIndex <= inputDataset.RasterCount; bandIndex++)
+            {
+                Band inputBand = inputDataset.GetRasterBand(bandIndex);
+                Band outputBand = outputDataset.GetRasterBand(bandIndex);
+
+                double[] buffer = new double[width * height];
+
+                inputBand.ReadRaster(xOffset, yOffset, width, height, buffer, width, height, 0, 0);
+
+                outputBand.WriteRaster(0, 0, width, height, buffer, width, height, 0, 0);
+            }
+        }
+    }
+}
